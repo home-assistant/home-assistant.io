fb85779a1b0def31c106d71bb1e00a55578ec952
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-03-27 16:53:44 -0700

    Fix repo url

--- plugins/environment_variables.rb
+++ plugins/environment_variables.rb
@@ -3,10 +3,15 @@ module Jekyll
     def generate(site)
       # https://www.netlify.com/docs/continuous-deployment/#build-environment-variables
       # These values will be available as {{ site.NLY_IS_PROD }}
-      site.config['NLY_REPOSITORY_URL'] = ENV['REPOSITORY_URL'] || 'https://github.com/home-assistant/home-assistant.github.io'
-      site.config['NLY_BRANCH'] = ENV['BRANCH'] || 'current'
-      site.config['NLY_CONTEXT'] = ENV['CONTEXT'] || 'production'
-      site.config['NLY_IS_PROD'] = site.config['NLY_CONTEXT'] == 'production'
+      repo_url = ENV['REPOSITORY_URL'] || 'https://github.com/home-assistant/home-assistant.github.io'
+
+      # Rewrite urls if repo url is the ssh format.
+      if repo_url.start_with? 'git@github.com:'
+        repo_url.sub! 'git@github.com:', 'https://github.com'
+      end
+
+      site.config['NLY_REPOSITORY_URL'] = repo_url
+      site.config['NLY_HEAD'] = ENV['HEAD'] || 'current'
     end
   end
 end
