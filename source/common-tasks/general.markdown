home-assistant/home-assistant.io

commit 41c17adbdaa730ee8645d90951bfaa5d795780e0

Speedtestdotnet - Remove section on manual polling (#28614)

source/common-tasks/general.markdown
@@ -6,3 +6,4 @@ installation_name: "Installation independent"
 This section provides tasks that do not depend on a specific Home Assistant installation type or a specific integration. They may be referenced in other procedures.
 
 {% include common-tasks/enable_entities.md %}
+{% include common-tasks/define_custom_polling.md %}

source/_integrations/goodwe.markdown
@@ -38,7 +38,7 @@ The best supported are the inverters of ET/EH families, where the sensors `Meter
 
 For the other inverter families, if such sensors are not directly available by the inverter, they can be calculated from existing sensors. [Template Sensor](/integrations/template/) can be used to separate buy and sell power values and [Riemann Sum](/integrations/integration/) can be used to convert these instant power (W) values into cumulative energy values (Wh), which then can be used within the energy dashboard.
 
-## Inverter polling frequency
+## Inverter polling interval
 
 The integration will poll the inverter for new values every 10 seconds. If you wish to receive fresh inverter data less (or more) frequently, you can disable the automatic refresh in the integration's system options (Enable polling for updates) and create your own automation with your desired polling frequency.
 
@@ -58,3 +58,9 @@ The integration will poll the inverter for new values every 10 seconds. If you w
 <div class='note'>
 It has been observed in some rare situations that frequent polling conflicts with updates to the Goodwe SEMS cloud portal and do not receive any updates anymore. Reducing polling frequency to 30 seconds or 1 minute seems to help in such cases.
 </div>
+
+For more detailed steps on how to define a custom polling interval, follow the procedure below.
+
+### Defining a custom polling interval
+
+{% include common-tasks/define_custom_polling.md %}

source/_integrations/google_travel_time.markdown
@@ -30,9 +30,6 @@ Notes:
 
 - Origin and Destination can be the address or the GPS coordinates of the location (GPS coordinates have to be separated by a comma). You can also enter an entity ID that provides this information in its state, an entity ID with latitude and longitude attributes, or zone friendly name (case sensitive).
 
-## Manual Polling
-
-Using automatic polling can lead to calls that exceed your API limit, especially when you are tracking multiple travel times using the same API key. To use more granular polling, disable automated polling in your config entry's System Options. To manually trigger a polling request, call the [`homeassistant.update_entity` service](/integrations/homeassistant/#service-homeassistantupdate_entity) as needed, either manually or via automations.
 
 ## Dynamic Configuration
 
@@ -65,7 +62,9 @@ Destination: Eddies House
 
 ## Updating sensors on-demand using Automation
 
-You can also use the `homeassistant.update_entity` service to update the sensor on-demand. For example, if you want to update `sensor.morning_commute` every 2 minutes on weekday mornings, you can use the following automation:
+Using automatic polling can lead to calls that exceed your API limit, especially when you are tracking multiple travel times using the same API key. To use more granular polling, disable automated polling.
+
+You can use the `homeassistant.update_entity` service to update the sensor on-demand. For example, if you want to update `sensor.morning_commute` every 2 minutes on weekday mornings, you can use the following automation:
 
 ```yaml
 - id: update_morning_commute_sensor
@@ -90,3 +89,9 @@ You can also use the `homeassistant.update_entity` service to update the sensor 
       target:
         entity_id: sensor.morning_commute
 ```
+
+For more detailed steps on how to define a custom polling interval, follow the procedure below.
+
+### Defining a custom polling interval
+
+{% include common-tasks/define_custom_polling.md %}

source/_integrations/landisgyr_heat_meter.markdown
@@ -68,8 +68,6 @@ Either heat usage or volume usage can be used as "Gas" on the energy dashboard. 
 
 Polling is by default done only once per day (and once right after adding the integration). Every time the Heat Meter values are read, battery time of the device will (supposedly) go down by about 30 minutes.
 
-For detailed control on polling and time of polling, consider disabling polling in the integration panel and poll manually. For instructions, see below.
-
 ### Polling manually (optional)
 
 For detailed control on when the device is polled, disable the default polling for this integration and create an automation that will update one of the entities (the other entities will be updated as well)
@@ -86,3 +84,9 @@ action:
     target:
       entity_id: sensor.heat_meter_heat_usage_gj
 ```
+
+For more detailed steps on how to define a custom polling interval, follow the procedure below.
+
+#### Defining a custom polling interval
+
+{% include common-tasks/define_custom_polling.md %}

source/_integrations/speedtestdotnet.markdown
@@ -22,9 +22,9 @@ The Speedtest.net integration uses the [Speedtest.net](https://speedtest.net/) w
 
 Most Speedtest.net servers require TCP port 8080 outbound to function. Without this port open you may experience significant delays or no results at all. See note on their [help page](https://www.speedtest.net/help).
 
-## Manual Polling
+By default, a speed test will be run every hour. You can disable polling using system options and use the `update_entity` service to automate the speed test frequency.
 
-By default, a speed test will be run every hour. To customize polling, you can disable automated polling. Go to {% my integrations title="**Settings** > **Devices & Services**" %}, and select the **Speedtest** integration. On the integration entry, select the three dots. Then, select **System options** and toggle the button to disable polling. To manually trigger a polling request, call the [`homeassistant.update_entity` service](/integrations/homeassistant/#service-homeassistantupdate_entity) as needed, either manually or via automations.
+{% include common-tasks/define_custom_polling.md %}
 
 ## Integration Sensors
 

source/_integrations/waze_travel_time.markdown
@@ -25,9 +25,9 @@ Notes:
 - The string inputs for `Substring *` allow you to force the integration to use a particular route or avoid a particular route in its time travel calculation. These inputs are case insensitive and matched against the description of the route.
 - When using the `Avoid Toll Roads?`, `Avoid Subscription Roads?` and `Avoid Ferries?` options, be aware that Waze will sometimes still route you over toll roads or ferries if a valid vignette/subscription is assumed. Default behavior is that Waze will route you over roads having subscription options. It is therefor best is to set both `Avoid Toll Roads?` and `Avoid Subscription Roads?` or `Avoid Ferries?` if needed and experiment to ensure the desired outcome.
 
-## Manual Polling
+## Defining a custom polling interval
 
-Some users want more control over polling intervals. To use more granular polling, you can disable automated polling. Go to {% my integrations title="**Settings** > **Devices & Services**" %}, and select the **Waze Travel Time** integration. On the integration entry, select the three dots. Then, select **System options** and toggle the button to disable polling. To manually trigger a polling request, call the [`homeassistant.update_entity` service](/integrations/homeassistant/#service-homeassistantupdate_entity) as needed, either manually or via automations.
+{% include common-tasks/define_custom_polling.md %}
 
 ## Example using dynamic destination
 

commit e5885f5bc5f9bcffacf183c0754a8715db070c20

Common tasks, installation independent: add missing title
- fixes error introduced in my previous PR #28614

source/common-tasks/general.markdown
@@ -6,4 +6,7 @@ installation_name: "Installation independent"
 This section provides tasks that do not depend on a specific Home Assistant installation type or a specific integration. They may be referenced in other procedures.
 
 {% include common-tasks/enable_entities.md %}
+
+## Defining a custom polling interval
+
 {% include common-tasks/define_custom_polling.md %}