075b6772480bda7b2ec5b755c2f8f062c874318a
Darren Griffin <darren.griffin@live.co.uk>
2025-07-31 09:18:12 +0000

    Remove unused animation

--- source/javascripts/connect/zwa-2/index.js
+++ source/javascripts/connect/zwa-2/index.js
@@ -3,7 +3,7 @@ import { ZWA2Animations } from "animations";
 import { ConnectHeader } from "header";
 
 new ZWA2RenderAnimation("scene1", "canvas.render-scroller#scene-one", ".animation-wrapper", 554);
-new ZWA2RenderAnimation("scene2", "canvas.render-scroller#scene-two", ".animation-wrapper", 554);
+//new ZWA2RenderAnimation("scene2", "canvas.render-scroller#scene-two", ".animation-wrapper", 554);
 new ConnectHeader();
 
 // if ?hide, display none on animation-wrapper
